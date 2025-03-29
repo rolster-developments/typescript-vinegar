ebfc4e687a6e326b26f66ffbe6eb674f8295c9df
ing.dacastillop <ing.dacastillop@gmail.com>
2025-03-29 18:08:22 -0500

    :sparkles: New Entity for manager refresh Models

--- src/datasource.ts
+++ src/datasource.ts
@@ -11,7 +11,7 @@ export abstract class AbstractEntityDataSource {
 
   abstract refresh(entity: TransactionRefresh): Promise<void>;
 
-  abstract update(model: AbstractModel, dirty?: DirtyModel): Promise<void>;
+  abstract update(model: AbstractModel, dirty: DirtyModel): Promise<void>;
 
   abstract delete(model: AbstractModel): Promise<void>;
 
